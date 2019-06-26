6f454e9dad1ebaa8d7ae26c88cf66f67dc2bfb33
takashiAg <crawd4274@gmail.com>
2019-06-26 23:19:33 +0900

    ボタンで送信できるようにした

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require("./crud.js");
 
+const fs = require('fs')
 
 let iframe_url = "https://www.google.co.jp/maps/"
 let api_url = "https://api.gnavi.co.jp/RestSearchAPI/v3/"
@@ -20,14 +21,44 @@ window.onload = function () {
 //     console.log(data)
 // }, 5000)
 
-find_ymap = async () => {
+find_ymap = async (wifi, outret) => {
     let {Lat, Lon} = ymap.getCenter()
-    let data = await find(Lat, Lon, 1, {
-        outret: 0,
-        wifi: 1
-    })
+    let opt = {
+        outret,
+        wifi
+    }
+    let data = await find(Lat, Lon, 1, opt)
     // let data = JSON.parse(d)
     table.results = data;
+
+    let file_data = "飲食店店名,ジャンル,立地,営業j時間,定休日・臨時定休日,コンセント,Wi-fi,禁煙,英語対応,エレベーター,座席数,他のサービスの導入,電話番号,住所,反応やアドバイス,外部リンクURL,FB,Twitter,インスタグラム\n";
+    for (let d of data) {
+        file_data += ""
+            + d.name + ","
+            + d.category + ","
+            + d.access.station.replace("\n", "") + "から徒歩" + d.access.walk + "分" + ","
+            + d.opentime.replace(/\r?\n/g, "") + ","
+            + ","
+            + ","
+            + ","
+            + ","
+            + ","
+            + ","
+            + ","
+            + ","
+            + d.tel + ","
+            + d.address + ","
+            + ","
+            + d.url + ","
+            + "\n"
+    }
+    console.log(file_data)
+    let filename
+        = document.getElementById("filename").value
+        + (opt.wifi == 1 ? "-wifi" : "")
+        + (opt.outret == 1 ? "-outret" : "")
+        + ".csv"
+    fs.writeFileSync("data/" + filename, file_data)
 }
 
 /*
@@ -49,7 +80,7 @@ let find = async (latitude, longitude, offset, option) => {
     d = JSON.parse(d)
     if (d.total_hit_count <= offset * 100)
         return d.rest
-    let ss = await find(latitude, longitude, offset + 1)
+    let ss = await find(latitude, longitude, offset + 1, option)
     return d.rest.concat(ss)
 }
 
