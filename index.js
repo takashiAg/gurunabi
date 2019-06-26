68578a1da919ddb168ac203b2ae6b16462d5f7dc
takashiAg <crawd4274@gmail.com>
2019-06-26 10:31:24 +0900

    ボタンで送信できるようにした

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require("./crud.js");
 
 let iframe_url = "https://www.google.co.jp/maps/"
 let api_url = "https://api.gnavi.co.jp/RestSearchAPI/v3/"
-let keyid = "d5560ce384cf654e0c24f5b5e95b4b66"
+let keyid = "6d85b8952e3fb799848e78a3b682109a"
 
 var ymap = null
 window.onload = function () {
@@ -12,23 +12,51 @@ window.onload = function () {
     ymap.drawMap(new Y.LatLng(35.66572, 139.73100), 17, Y.LayerSetId.NORMAL);
 }
 
-setInterval(async () => {
+// setInterval(async () => {
+//     let {Lat, Lon} = ymap.getCenter()
+//     let d = await find(Lat, Lon)
+//     let data = JSON.parse(d)
+//     table.results = data.rest;
+//     console.log(data)
+// }, 5000)
+
+find_ymap = async () => {
     let {Lat, Lon} = ymap.getCenter()
-    let d = await find(Lat, Lon)
-    console.log(d)
-}, 5000)
+    let data = await find(Lat, Lon)
+    // let data = JSON.parse(d)
+    table.results = data;
+
+}
 
 /*
  * latitude  : 緯度
  * longitude : 経度
  */
-let find = async (latitude, longitude) => {
+let find = async (latitude, longitude, offset) => {
+    if (typeof offset !== typeof 1)
+        offset = 1
     let data = {
         keyid, latitude, longitude,
-        range: 1
+        range: 1,
+        offset_page: offset,
+        hit_per_page: 100
     }
-    return await http.get(api_url, data)
+    let d = await http.get(api_url, data)
+    d = JSON.parse(d)
+    if (d.total_hit_count <= offset * 100)
+        return d.rest
+    let ss = await find(latitude, longitude, offset + 1)
+    return d.rest.concat(ss)
 }
+
+// let find = async (latitude, longitude, offset) => {
+//     let data = {
+//         keyid, latitude, longitude,
+//         range: 1,
+//         hit_per_page: 100
+//     }
+//     return await http.get(api_url, data)
+// }
 
 // let index = async () => {
 //     let data = await find(12.3, 123.4);
